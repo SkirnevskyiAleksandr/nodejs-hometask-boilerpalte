89d2ce2acbf3a3e03237b500b82c2935fb61e69d
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-01-01 22:36:38 +0200

    create user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,15 @@ const { createUserValid, updateUserValid } = require('../middlewares/user.valida
 const { responseMiddleware } = require('../middlewares/response.middleware');
 
 const router = Router();
+router.get('', function (req, res) {
+    let allUsers = UserService.getAllUsers()
+    res.send(allUsers)
+})
+
+router.post('', function (req, res) {
+    let addUser = UserService.addUser(req.body)
+    res.send(addUser)
+})
 
 // TODO: Implement route controllers for user
 
--- services/userService.js
+++ services/userService.js
@@ -2,11 +2,29 @@ const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
 
+    // const getName = user => user ? user.name : null;
+
+    addUser(data) {
+        const item = UserRepository.create(data)
+        if (!item) {
+            return null;
+        }
+        return item
+    }
+
+    getAllUsers() {
+        const item = UserRepository.getAll()
+        if (!item) {
+            return null;
+        }
+        return item;
+    }
+
     // TODO: Implement methods to work with user
 
     search(search) {
         const item = UserRepository.getOne(search);
-        if(!item) {
+        if (!item) {
             return null;
         }
         return item;
